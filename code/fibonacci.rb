a28ca5f63628b1b36e47a3c337a47c014d970326
stbarrientos <stbarrientos@gmail.com>
2014-10-07 17:09:35 -0400

    finished fibonacci exercise

--- code/fibonacci.rb
+++ code/fibonacci.rb
@@ -2,6 +2,19 @@ class Fibonacci
   # Implement a method that will calculate the Nth number of the Fibonacci
   # sequence (http://en.wikipedia.org/wiki/Fibonacci_number)
   def self.calculate(n)
+  	# Assign x and y to 0 and 1, respctively. This is the beggining of the
+  	# modern Fibonacci sequence as described by wikipedia
+  	x,y = 0,1
+
+  	# x is assigned the value of y and y is assigned the sum of x and y
+  	# the reason behind the dual assignment is that, if set individually,
+  	# the value of x will be changed by the time y needs to be reassigned
+  	n.times { |i| x,y = y, y + x }
+  	
+  	#return y
+  	"Number at #{n}: #{y}"
   end
 
 end
+
+puts Fibonacci.calculate(7)
