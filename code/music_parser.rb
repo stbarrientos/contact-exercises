311e660f459286e5039d8b3e02f4d67b3aa548e4
stbarrientos <stbarrientos@gmail.com>
2014-10-08 08:46:53 -0400

    yaml: 2/4 passing

--- code/music_parser.rb
+++ code/music_parser.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class MusicParser
   # Implement a method parse the contents of a YAML file and return
   # an object whose values are accessible using the [] operator or method calls
@@ -5,5 +7,9 @@ class MusicParser
   # Note: Use of the YAML library is fine (encouraged, even) but please don't
   #       use any third-party gems that enable the required functionality.
   def self.parse(filename)
+  	file = open(filename, &:read)
+  	y = YAML.load(file)
   end
 end
+
+MusicParser.parse("data/music.yaml")
